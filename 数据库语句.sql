59c67a6d2d5e835beed18c372fff806b7b713d12
Stellar79 <89825380+Stellar79@users.noreply.github.com>
2022-05-06 21:32:51 +0800

    sql

---
+++ 数据库语句.sql
@@ -0,0 +1,16 @@
+create table Course
+(
+	id INT(11) AUTO_INCREMENT, 
+  lastname    VARCHAR(20) null,
+  firstname    varchar(20) null,
+	mi VARCHAR(10) NULL,
+	email VARCHAR(100) NULL,
+	city VARCHAR(20) NULL,
+	state VARCHAR(20) null,
+	zip VARCHAR(20) NULL,
+	telephone VARCHAR(20) NULL,
+	street VARCHAR(500) NULL,
+  constraint Course_id_uindex PRIMARY KEY (id),
+  unique (id)
+)
+  engine = InnoDB;
